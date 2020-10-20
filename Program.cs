eea86af42d8d9c61138c048e4a83aa374b52f97a
Marcus Kuhn <marcus@riacus-music.de>
2020-10-20 11:24:58 +0200

    Test

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+using OpenTK;
+
+namespace CG_Projekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Window window = new Window();
+            window.WindowState = WindowState.Maximized;
+
+            window.Run();
+        }
+    }
+}
