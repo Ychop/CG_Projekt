b5ba2f4a1fa3ffd5fed16513abbb1202860b557d
Marcus Kuhn <marcus@riacus-music.de>
2021-01-13 14:05:08 +0100

    Enemey only chase player if in distignuesd radius. Ka wie man Distigushed schreibt.

--- Models/Enemy.cs
+++ Models/Enemy.cs
@@ -1,7 +1,7 @@
 ﻿using OpenTK;
 using OpenTK.Graphics.OpenGL;
 using System.Drawing;
-using System.Collections.Generic;
+using System;
 
 namespace CG_Projekt.Models
 {
@@ -19,11 +19,17 @@ namespace CG_Projekt.Models
 
         public void EnemyAI(Enemy enemy, Player player, float deltaTime)
         {
-            this.Velocity = deltaTime * 0.007f;
-            Vector2 playerDirection = new Vector2(player.Position.X - enemy.Position.X, player.Position.Y - enemy.Position.Y);
-            playerDirection.Normalize(); // Ohne Normalize würden sich die gegner schneller zum spieler bewegen, je weiter sie von ihm weg sind weg sind
-            enemy.Position += playerDirection * Velocity;
+
+            if((Math.Pow(enemy.Position.X - player.Position.X, 2) + Math.Pow(enemy.Position.Y - player.Position.Y, 2)) < 0.2f)
+            {
+                this.Velocity = deltaTime * 0.007f;
+                Vector2 playerDirection = new Vector2(player.Position.X - enemy.Position.X, player.Position.Y - enemy.Position.Y);
+                playerDirection.Normalize(); // Ohne Normalize würden sich die gegner schneller zum spieler bewegen, je weiter sie von ihm weg sind weg sind
+                enemy.Position += playerDirection * Velocity;
+            }
+             
         }
+
 
 
         public void EnemyHelath(Enemy enemy)
