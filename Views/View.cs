f807dd6135a89db50d1a99dd71f3e0789d6ed1f1
Sebastian Jaeger <sebastianjaeger1984@hotmail.de>
2021-01-17 15:16:51 +0100

    Texturen hinzugefügt für hintergrund und spieler

--- Views/View.cs
+++ Views/View.cs
@@ -12,12 +12,22 @@ namespace CG_Projekt
     {
 
         private readonly int texPlayer;
+        private readonly int texEnemy;
+        private readonly int texObstacle;
+        private readonly int texCollectible;
+        private readonly int texBullet;
+        private readonly int texFloor;
 
         public View(Camera camera)
         {
             Camera = camera;
             var content = $"{nameof(CG_Projekt)}.Content.";
             texPlayer = Texture.Load(Resource.LoadStream(content + "player.jpg"));
+            texEnemy = Texture.Load(Resource.LoadStream(content + "monster.jpg"));
+            texObstacle = Texture.Load(Resource.LoadStream(content + "rocks.png"));
+            texCollectible = Texture.Load(Resource.LoadStream(content + "collectible.jpg"));
+            texBullet = Texture.Load(Resource.LoadStream(content + "bullet.jpg"));
+            texFloor = Texture.Load(Resource.LoadStream(content + "grass.jpg"));
             GL.Enable(EnableCap.Texture2D);
             GL.BlendFunc(BlendingFactor.SrcAlpha, BlendingFactor.OneMinusSrcAlpha);
             GL.Enable(EnableCap.Blend);
@@ -84,14 +94,10 @@ namespace CG_Projekt
         internal void DrawLevel()
         {
             GL.Begin(PrimitiveType.Quads);
-            GL.Color3(Color.White);
-            GL.TexCoord2(-0.9f, -0.9f);
+            //GL.Color3(Color.White);
             GL.Vertex2(-0.9f, -0.9f);
-            GL.TexCoord2(0.9f, -0.9f);
             GL.Vertex2(0.9f, -0.9f);
-            GL.TexCoord2(0.9f, 0.9f);
             GL.Vertex2(0.9f, 0.9f);
-            GL.TexCoord2(-0.9f, 0.9f);
             GL.Vertex2(-0.9f, 0.9f);
             GL.End();
         }
@@ -101,16 +107,17 @@ namespace CG_Projekt
             var i = 0;
             foreach (LevelGrid levelGrid in model.levelGrids)
             {
-                GL.Color3(model.levelGrids[i]._color);
+                GL.BindTexture(TextureTarget.Texture2D, texFloor);
+                //GL.Color3(model.levelGrids[i]._color);
                 GL.Begin(PrimitiveType.Quads);
-                GL.TexCoord2(model.levelGrids[i]._position);
+                GL.TexCoord2(new Vector2(0, 0));
                 GL.Vertex2(model.levelGrids[i]._position);
-                GL.TexCoord2(model.levelGrids[i]._position + new Vector2(0.017f, 0));
-                GL.Vertex2(model.levelGrids[i]._position + new Vector2(0.017f, 0));
-                GL.TexCoord2(model.levelGrids[i]._position + new Vector2(0.017f, 0.017f));
-                GL.Vertex2(model.levelGrids[i]._position + new Vector2(0.017f, 0.017f));
-                GL.TexCoord2(model.levelGrids[i]._position + new Vector2(0, 0.017f));
-                GL.Vertex2(model.levelGrids[i]._position + new Vector2(0, 0.017f));
+                GL.TexCoord2(new Vector2(1, 0));
+                GL.Vertex2(model.levelGrids[i]._position + new Vector2(0.018f, 0));
+                GL.TexCoord2(new Vector2(1, 1));
+                GL.Vertex2(model.levelGrids[i]._position + new Vector2(0.018f, 0.018f));
+                GL.TexCoord2(new Vector2(0, 1));
+                GL.Vertex2(model.levelGrids[i]._position + new Vector2(0, 0.018f));
                 GL.End();
 
                 i++;
@@ -119,18 +126,19 @@ namespace CG_Projekt
 
         internal void DrawPlayer(Model model)
         {
+            GL.BindTexture(TextureTarget.Texture2D, texPlayer);
             GL.PushMatrix();
             GL.Translate(new Vector3(model.player.Position.X, model.player.Position.Y, 0));
             GL.Rotate(model.player.Angle, new Vector3d(0, 0, -1));
-            GL.Color3(model.player.Color);
+            //GL.Color3(model.player.Color);
             GL.Begin(PrimitiveType.Quads);
-            GL.TexCoord2(new Vector2(-model.player.Size, -model.player.Size));
+            GL.TexCoord2(new Vector2(1, 1));
             GL.Vertex2(new Vector2(-model.player.Size, -model.player.Size));
-            GL.TexCoord2(new Vector2(model.player.Size, -model.player.Size));
+            GL.TexCoord2(new Vector2(0, 1));
             GL.Vertex2(new Vector2(model.player.Size, -model.player.Size));
-            GL.TexCoord2(new Vector2(model.player.Size, model.player.Size));
+            GL.TexCoord2(new Vector2(0, 0));
             GL.Vertex2(new Vector2(model.player.Size, model.player.Size));
-            GL.TexCoord2(new Vector2(-model.player.Size, model.player.Size));
+            GL.TexCoord2(new Vector2(1, 0));
             GL.Vertex2(new Vector2(-model.player.Size, model.player.Size));
             GL.End();
             GL.PopMatrix();
@@ -139,11 +147,17 @@ namespace CG_Projekt
         {
             foreach (Bullet bullet in model.bullets)
             {
-                GL.Color3(Color.Black);
+                GL.BindTexture(TextureTarget.Texture2D, texBullet);
+                //GL.Color3(Color.Black);
                 GL.Begin(PrimitiveType.Quads);
+
+                GL.TexCoord2(bullet.Position + new Vector2(-bullet.Size, -bullet.Size));
                 GL.Vertex2(bullet.Position + new Vector2(-bullet.Size, -bullet.Size));
+                GL.TexCoord2(bullet.Position + new Vector2(bullet.Size, -bullet.Size));
                 GL.Vertex2(bullet.Position + new Vector2(bullet.Size, -bullet.Size));
+                GL.TexCoord2(bullet.Position + new Vector2(bullet.Size, bullet.Size));
                 GL.Vertex2(bullet.Position + new Vector2(bullet.Size, bullet.Size));
+                GL.TexCoord2(bullet.Position + new Vector2(-bullet.Size, bullet.Size));
                 GL.Vertex2(bullet.Position + new Vector2(-bullet.Size, bullet.Size));
                 GL.End();
             }
@@ -154,7 +168,8 @@ namespace CG_Projekt
             foreach (Enemy enemy in model.enemies)
             {
 
-                GL.Color3(enemy.Color);
+                //GL.Color3(enemy.Color);
+                
                 GL.Begin(PrimitiveType.Quads);
                 GL.Vertex2(model.enemies[i].Position + new Vector2(-model.enemies[i].Size, -model.enemies[i].Size));
                 GL.Vertex2(model.enemies[i].Position + new Vector2(model.enemies[i].Size, -model.enemies[i].Size));
@@ -168,7 +183,7 @@ namespace CG_Projekt
             i = 0;
             foreach (Obstacle obstacle in model.obstacles)
             {
-                GL.Color3(obstacle.Color);
+               // GL.Color3(obstacle.Color);
                 GL.Begin(PrimitiveType.Quads);
                 GL.Vertex2(model.obstacles[i].Position + new Vector2(-model.obstacles[i].Size, -model.obstacles[i].Size));
                 GL.Vertex2(model.obstacles[i].Position + new Vector2(model.obstacles[i].Size, -model.obstacles[i].Size));
@@ -180,7 +195,7 @@ namespace CG_Projekt
             i = 0;
             foreach (PickUp pickup in model.pickUps)
             {
-                GL.Color3(pickup.Color);
+                //wGL.Color3(pickup.Color);
                 GL.Begin(PrimitiveType.Quads);
                 GL.Vertex2(model.pickUps[i].Position + new Vector2(-model.pickUps[i].Size, -model.pickUps[i].Size));
                 GL.Vertex2(model.pickUps[i].Position + new Vector2(model.pickUps[i].Size, -model.pickUps[i].Size));
