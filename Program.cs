6f167ad3fb22edcd438af3b5b17e0593e9d39b38
Marcus Kuhn <marcus@riacus-music.de>
2021-08-06 19:50:13 +0200

    Added Programm.cs

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace CG_Projekt
+{
+    using OpenTK;
+    internal class Program
+    {
+
+        private static void Main()
+        {
+            var window = new GameWindow();
+            var camera = new Camera();
+            var model = new Model();
+            var view = new View(camera);
+            var controller = new Controller(view, model);
+            window.Title = "Topdown-Shooter";
+            window.MouseMove += (_, args) => controller.TranslateMouseCoordinates(args.X, window.Height - 1 - args.Y);
+            window.KeyPress += (_, args) => controller.WepaonSelection(args.KeyChar);
+            window.UpdateFrame += (_, __) =>
+            {
+                controller.Update((float)__.Time);
+            };
+            window.WindowState = WindowState.Maximized;
+            window.Resize += (_, __) => view.Resize(window.Width, window.Height);
+            window.RenderFrame += (_, __) => view.Draw(model);
+            window.RenderFrame += (_, __) => window.SwapBuffers();
+            window.Run();
+        }
+    }
+}
